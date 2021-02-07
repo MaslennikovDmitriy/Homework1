cae9f1b3c6865fc7090af39672e71f2aea1f6862
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-02-07 17:44:13 +0300

    Deleting all previous tests

--- Homework1.cpp
+++ Homework1.cpp
@@ -5,19 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-    cout << "Added first changes" << endl;
-    cout << "Index Editor testing..." << endl;
-    int a = 10, b = 5;
-    cout << "a + b = " << a + b << endl;
-    cout << "a - b = " << a - b << endl;
-    cout << "a * b = " << a * b << endl;
-    cout << "a / b = " << a / b << endl;
-<<<<<<< HEAD
-    cout << "test2" << endl;
-=======
 
-    cout << "master branch test2" << endl;
->>>>>> > master
-cout << "test2 another change" << endl;
     return EXIT_SUCCESS; 
 }
