fc4c77e22320a9f35737737d9239317105029519
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-02-07 17:45:49 +0300

    first change

--- Homework1.cpp
+++ Homework1.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+<<<<<<< HEAD
 
     cout << "changing the master branch" << endl;
+=======
+    cout << "start testing..." << endl;
+>>>>>>> 0476f06 (first change)
     return EXIT_SUCCESS; 
 }
