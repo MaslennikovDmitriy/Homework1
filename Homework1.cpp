ac19fde9d6e12d9cfc0a134b574d278c373aeb88
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-02-07 17:38:50 +0300

    test2

---
+++ Homework1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Added first changes" << endl;
+    cout << "Index Editor testing..." << endl;
+    int a = 10, b = 5;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "test2" << endl;
+    return EXIT_SUCCESS;
+}
