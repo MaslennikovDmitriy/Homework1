efb2af4f4bc75fdc4aa72d92fa56973891ce8922
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-02-07 17:52:33 +0300

    deleting again...

--- Homework1.cpp
+++ Homework1.cpp
@@ -5,12 +5,5 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-<<<<<<< HEAD
-
-    cout << "changing the master branch" << endl;
-=======
-    cout << "start testing..." << endl;
->>>>>> > 0476f06(first change)
-cout << "adding another string" << endl;
     return EXIT_SUCCESS; 
 }
